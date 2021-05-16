bc30bad76fc1a5cbe75cfa14d5124f79a49b4e58
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2021-05-16 16:25:31 +0300

    Update FrontendScript.sh

--- FrontendScript.sh
+++ FrontendScript.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker stop Frontendcont  
-docker rm Frontendcont
 docker build -t frontimg2 .
 docker run -i --name Frontendcont --network=gomathanetwork --ip 192.168.10.40  -p 5050:5050  frontimg2 /bin/bash
 docker run -it --name Frontendcont frontimg2 /bin/bash
